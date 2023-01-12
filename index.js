bd271aa7a3c324e8d57b9138d42d61ce62f1fc8d
charlescrabtree <charles.crabtree.horn@gmail.com>
2023-01-12 10:37:11 -0800

    added better solution

--- index.js
+++ index.js
@@ -16,3 +16,15 @@ for(let i = ".".length;i <= (max * max);i++){
 console.log(array.join('\r\n'));
 }
 fizzbuzz();
+
+for (let i = 1; i < 100; i++) {
+    if (i % 3 && i % 5 === 0) {
+        console.log("fizzbuzz");
+    } else if (i % 5 === 0) {
+        console.log("buzz");
+    } else if (i % 3 === 0) {
+        console.log("fizz");
+    } else {
+        console.log(i);
+    }
+}
